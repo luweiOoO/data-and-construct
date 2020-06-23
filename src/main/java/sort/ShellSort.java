47544b91ef37bb994b5e3dfb98125a27039b78bb
luwei <luwei09@sinochem.com>
2020-06-23 14:29:02 +0800

    希尔排序-移位法

--- src/main/java/sort/ShellSort.java
+++ src/main/java/sort/ShellSort.java
@@ -11,13 +11,13 @@ public class ShellSort {
 
     public static void main(String[] args){
         int[] arr = {8,9,1,7,2,3,5,4,6,0};
-        sort(arr);
+        sort2(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     //使用逐步推导的方式来编写希尔排序
+    //交换法
     public static void sort(int[] arr){
-
         int temp = 0;
         for(int gap = arr.length / 2; gap > 0; gap /= 2){
             for(int i = gap; i < arr.length; i++){
@@ -30,6 +30,27 @@ public class ShellSort {
                 }
             }
         }
+    }
 
+    //移位法
+    public static void sort2(int[] arr){
+
+        //增量gap，并逐步的缩小增量
+        for(int gap = arr.length / 2;gap > 0; gap /= 2){
+            //从第gap个元素，逐个对其所在的组直接进行插入排序
+            for(int i = gap; i < arr.length; i++){
+                int j = i;
+                int temp = arr[j];
+                if(arr[j] < arr[j - gap]){
+                    while(j - gap >= 0 && temp <= arr[j - gap]){
+                        //移动
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    //当退出while后，就给temp找到插入的位置
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
